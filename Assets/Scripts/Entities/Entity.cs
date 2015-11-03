25bd2e04c7704f97007cbb2e1ce2a5467cca34f7
Mats Stenhaug <matsstenhaug@hotmail.com>
2015-11-03 16:29:14 +0100

    Sex

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -18,49 +18,49 @@ public class Entity : MonoBehaviour
 	 * 
 	 * Resource Consumption
 	 */
-
+	
 	/*
 		Have a variable+threshold for how often it is a male and female?
 	 */
-
+	
 	public Entity partner = null;
-
+	
 	public ArrayList children = new ArrayList ();
-
+	
 	public bool isPregnant = false;
 	public bool hasSexPref = false;
 	public bool hasLived = false;
-
+	
 	public sex s;
 	public sexPref sp;
 	public int age;
 	public int genderThresh;
-
+	
 	public float looks;
 	public float strength;
 	public float stamina;
 	public float hp;
 	public float intelligence;
 	public float potency;
-
+	
 	public float resourceConsumpt;
-
+	
 	public int sexPrefThresh; // also used for when he/she is of age (able to have a child)
-
+	
 	public ArrayList immunities;
 	public ArrayList weaknesses;
 	public ArrayList handicaps;
-
-
-
+	
+	
+	
 	public enum sex {
 		male, female
 	}
-
+	
 	public enum sexPref {
 		hetero, bi, homo, a
 	}
-
+	
 	public Entity(int genderThreshold, float strength, ArrayList immunities, 
 	              float stamina, float hp, int sexPrefThresh, float intelligence,
 	              ArrayList weaknesses, ArrayList handicaps, float potency){
@@ -75,27 +75,22 @@ public class Entity : MonoBehaviour
 		this.weaknesses = weaknesses;
 		this.handicaps = handicaps;
 		this.potency = potency;
-
-	}
-
-	// Use this for initialization
-	void Start ()
-	{
-		// setting the gender of the entity
+		
 		int gend = Random.Range (1,101);
 		if (gend >= genderThresh) {
 			s = sex.male;
 		}else{
 			s = sex.female;
 		}
-
 		
-
+		//Debug.Log ("Gender: " + s);
+		
 	}
-
+	
+	
 	public void setSexPref(){
 		// Might need a variable for each preferance, that can be tweeked with Evo-Alg
-
+		
 		int a = Random.Range(0,4);
 		switch(a){
 		case 0:
@@ -111,25 +106,25 @@ public class Entity : MonoBehaviour
 			sp = sexPref.bi; // 50/50 for wanting opposite sex
 			break;
 		}
+		
+		//Debug.Log ("Sex Pref = " + sp);
 	}
-
+	
 	public void IncreaseAge(){
 		age++;
 		if(age >= sexPrefThresh && !hasSexPref)
 		{
 			setSexPref();
+			hasSexPref = true;
 		}
 	}
-
-	public void findPartner(){
-		
-	}
+	
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		findPartner (); // create in main class, so that it goes through all the entities.
-
+		
+		
 	}
 }
 
--- Assets/Scripts/VillageLifeSimulator.cs
+++ Assets/Scripts/VillageLifeSimulator.cs
@@ -4,20 +4,24 @@ using System.Collections;
 
 public class VillageLifeSimulator : MonoBehaviour
 {
-
+	
 	ArrayList ents;
-
+	ArrayList children;
+	int iteration = 0;
+	
 	// Use this for initialization
 	void Start ()
 	{
 		ents = new ArrayList ();
+		children = new ArrayList ();
 		// creates 20 Entities.
 		for (int i = 0; i < 20; i ++) {
 			Entity e = new Entity(50,Random.Range(1,101),null,Random.Range(1,101),100,Random.Range(12,18),Random.Range(1,1001),null,null,Random.Range(1,101));
 			ents.Add(e);
 		}
+		Debug.Log ("Created " + ents.Count + " Entities.");
 	}
-
+	
 	void UpdateInteractions(){
 		foreach(Entity e in ents) {
 			e.hasLived = true;
@@ -27,29 +31,39 @@ public class VillageLifeSimulator : MonoBehaviour
 			// give birth to new children.
 			// Make the child dependant on the parents??
 			if(e.isPregnant){
-				Entity newEnt = new Entity(50,Random.Range(1,101),null,Random.Range(1,101),100,Random.Range(12,18),Random.Range(1,1001),null,null,Random.Range(1,101));
+				//Debug.Log("Child Created");
+				Entity newEnt = new Entity(50,Random.Range(1,101),null,Random.Range(1,101),100,Random.Range(13,18),Random.Range(1,1001),null,null,Random.Range(1,101));
 				e.isPregnant = false;
 				e.children.Add(newEnt);
 				e.partner.children.Add(newEnt);
+				children.Add(newEnt);
 			}
 			e.hasLived = false; // resets and makes ready for next iteration
+			e.IncreaseAge();
+			if(e.age >80){
+				ents.Remove(e);
+				Destroy(e);
+			}
 		}
 	}
-
+	
 	// add STDs?
 	void makeLoveChild(Entity e, Entity e2){
 		if(e2.partner == null || e2.partner == e){
 			if(Random.Range(1,101)>=e.potency){
 				if(Random.Range(1,101)>=e2.potency){
+					//Debug.Log("HAVING SEX");
 					// Potency Success -> Pregnancy inc!
-					if(e.s == Entity.sex.female){
-						e.isPregnant = true;
-						e.partner = e2;
-						e2.partner = e;
-					}else{
-						e2.isPregnant = true;
-						e.partner = e2;
-						e2.partner = e;
+					if(!e2.isPregnant && !e.isPregnant){
+						if(e.s == Entity.sex.female){
+							e.isPregnant = true;
+							e.partner = e2;
+							e2.partner = e;
+						}else{
+							e2.isPregnant = true;
+							e.partner = e2;
+							e2.partner = e;
+						}
 					}
 				}
 			}
@@ -57,21 +71,21 @@ public class VillageLifeSimulator : MonoBehaviour
 	}
 	// add STDs?
 	void makeLove(Entity e, Entity e2){
-	
+		
 	}
-
+	
 	void meetPeople(Entity e){
 		if (e.hasSexPref) {
 			foreach (Entity e2 in ents) {
 				//a sexual = break
 				if (e.sp == Entity.sexPref.a) {
-					break;
+					//break;
 				}
-
+				
 				if (e2.sp == Entity.sexPref.a) {
-					break;
+					//break;
 				}
-
+				
 				// MAKE SURE YOU DONT HAVE INCEST!! :P
 				// Different sex
 				if (e2.s != e.s) {
@@ -79,18 +93,26 @@ public class VillageLifeSimulator : MonoBehaviour
 					switch (e.sp) {
 					case Entity.sexPref.bi:
 						if (e2.sp == Entity.sexPref.bi || e2.sp == Entity.sexPref.hetero) {
-							makeLoveChild (e, e2);
+							if(e.age < 42 && e2.age <42){
+								makeLoveChild (e, e2);
+							}else{
+								makeLove(e,e2);
+							}
 						}
 						break;
 					case Entity.sexPref.hetero:
 						if (e2.sp == Entity.sexPref.bi || e2.sp == Entity.sexPref.hetero) {
-							makeLoveChild (e, e2);
+							if(e.age < 42 && e2.age <42){
+								makeLoveChild (e, e2);
+							}else{
+								makeLove(e,e2);
+							}
 						}
 						break;
 					}
 				}
-			//Same Sex
-			else {
+				//Same Sex
+				else {
 					//Bi or Homo
 					switch (e.sp) {
 					case Entity.sexPref.bi:
@@ -108,14 +130,25 @@ public class VillageLifeSimulator : MonoBehaviour
 			}
 		}
 	}
-
+	
 	void Update ()
 	{
+		
 		// Update interactions, update deceases, update people.
 		UpdateInteractions ();
-
+		ArrayList newList = new ArrayList ();
+		foreach (Entity e in ents) {
+			newList.Add(e);
+		}
+		foreach (Entity e in children) {
+			newList.Add(e);
+		}
+		children = new ArrayList ();
+		ents = (ArrayList)newList.Clone ();
+		iteration++;
+		Debug.Log ("Iteration: "+iteration+". Entities: "+ents.Count+".");
 		//findPartner (); // create in main class, so that it goes through all the entities.
-
+		
 	}
 }
 
