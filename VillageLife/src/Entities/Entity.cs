e641eeec13eac6bd785d8e07979d80485e10da6f
Mats Stenhaug <Mats Stenhaug>
2015-11-03 13:25:06 +0100

    fixing Entity.cs

--- VillageLife/src/Entities/Entity.cs
+++ VillageLife/src/Entities/Entity.cs
@@ -19,12 +19,40 @@ public class Entity : MonoBehaviour
 	 * Resource Consumption
 	 */
 
+	/*
+		Have a variable+threshold for how often it is a male and female?
+	 */
 
+	sex s;
+	int age;
+	float sexThresh;
+	float strength;
+	ArrayList<Decease> immunities;
+
+
+	enum sex {
+		male, female
+	}
+
+	public Entity(int sexThreshold, float strength, ArrayList<Decease> immunities, 
+	              float stamina, float hp, int sexPrefThresh, float intelligence,
+	              ArrayList<Decease> weaknesses, ArrayList<Decease> handicap, float potency){
+
+	}
 
 	// Use this for initialization
 	void Start ()
 	{
-	
+		Random r = new Random ();
+		int a = r.Next (2);
+
+		if (a == 1) {
+			s = sex.male;
+		}else{
+			s = sex.female;
+		}
+
+
 	}
 	
 	// Update is called once per frame
